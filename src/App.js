d0964a11ab830046d6d734d6ec1108a61ff5cb44
Omoalfa <59462494+Omoalfa@users.noreply.github.com>
2021-04-07 11:59:26 +0100

    react boiler plate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/index.css'
+import './styles/app.css'
+import React from 'react'
+
+const App = () => (
+    <div>
+        <h1>Hello React</h1>            
+    </div>
+)
+
+
+export default App
